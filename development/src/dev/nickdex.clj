447688030ff5764eedada15d30e6f2f60546ae60
Nikhil Warke <nwarke@proton.me>
2024-01-31 15:42:01 +0530

    feat(dev): Add portal logic for development

---
+++ development/src/dev/nickdex.clj
@@ -0,0 +1,13 @@
+(ns dev.nickdex
+  (:require [portal.api :as p]))
+
+(def portal-atom (atom nil))
+
+(defn launch-portal []
+  (let [portal (p/open (or @portal-atom {:launcher :vs-code}))]
+    (reset! portal-atom portal)
+    (add-tap #'p/submit)
+    portal))
+
+(comment
+  (launch-portal))
