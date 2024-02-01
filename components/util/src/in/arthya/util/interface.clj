a00a978cf08e0023ef5681f76ff7592426cf896b
Nikhil Warke <nwarke@proton.me>
2024-02-01 22:48:08 +0530

    fix(util): handle nil case in includes-any?

--- components/util/src/in/arthya/util/interface.clj
+++ components/util/src/in/arthya/util/interface.clj
@@ -2,10 +2,13 @@
   (:require [clojure.string :as str]))
 
 (defn includes-any?
-  "Return true if any of the strings matches the attribute"
+  "Return true if any of the strings matches the attribute, false otherwise.
+   In case attribute is nil, return false"
   [attribute strings]
-  (let [lower-category (str/lower-case attribute)]
-    (some #(str/includes? lower-category (str/lower-case %)) strings)))
+  (if (nil? attribute)
+    false
+    (let [lower-category (str/lower-case attribute)]
+      (some #(str/includes? lower-category (str/lower-case %)) strings))))
 
 (defn contains-partial?
   "Return true if any of rows partially contains the search string"
