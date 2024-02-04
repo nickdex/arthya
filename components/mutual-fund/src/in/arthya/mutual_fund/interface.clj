60b924b05233821c59dbf88bd85856741adde7d2
Nikhil Warke <nwarke@proton.me>
2024-02-04 06:49:57 +0530

    feat: Add mutual-fund component
    For consuming mutual fund transaction source
    
    t

---
+++ components/mutual-fund/src/in/arthya/mutual_fund/interface.clj
@@ -0,0 +1,5 @@
+(ns in.arthya.mutual-fund.interface)
+
+(defn parse
+  "Converts transaction records from mutual fund statement to common transaction structure. Expects list of list"
+  [records])
---
+++ components/mutual-fund/test/in/arthya/mutual_fund/interface_test.clj
@@ -0,0 +1,6 @@
+(ns in.arthya.mutual-fund.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [in.arthya.mutual-fund.interface :as mutual-fund]))
+
+(deftest dummy-test
+  (is (= 1 1)))
