e4bdc2ab4213805d6986308fab3093826d92e27c
Nikhil Warke <nwarke@proton.me>
2024-02-09 11:57:22 +0530

    feat(parser): Add parser component
    Parser is responsible for consuming all sources of transactions
    and giving common structure for further processing

---
+++ components/parser/src/in/arthya/parser/interface.clj
@@ -0,0 +1,5 @@
+(ns in.arthya.parser.interface)
+
+(defn parse
+  "Converts transaction records from bank statement to common transaction structure. Expects list of list"
+  [records])
---
+++ components/parser/test/in/arthya/parser/interface_test.clj
@@ -0,0 +1,6 @@
+(ns in.arthya.parser.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [in.arthya.parser.interface :as parser]))
+
+(deftest dummy-test
+  (is (= 1 1)))
