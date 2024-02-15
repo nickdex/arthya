38f4d91eeef5f40ddca11af07ddd73e554968d1b
Nikhil Warke <nwarke@proton.me>
2024-02-16 04:00:26 +0530

    feat(parser): Add Sodexo parser
    sodexo

--- components/parser/src/in/arthya/parser/interface.clj
+++ components/parser/src/in/arthya/parser/interface.clj
@@ -3,11 +3,13 @@
 and giving common structure for further processing"
   (:require
    [in.arthya.parser.icici-bank :as icici-bank]
-   [in.arthya.parser.icici-credit-card :as icici-cc]))
+   [in.arthya.parser.icici-credit-card :as icici-cc]
+   [in.arthya.parser.sodexo :as sodexo]))
 
 (defn parse
   "Converts transaction records from source to common transaction structure. Expects list of list"
   [source records]
   (condp = source
     :icici-bank (icici-bank/parse records)
-    :icici-cc (icici-cc/parse records)))
+    :icici-cc (icici-cc/parse records)
+    :sodexo (sodexo/parse records)))
---
+++ components/parser/src/in/arthya/parser/sodexo.clj
@@ -0,0 +1,45 @@
+(ns in.arthya.parser.sodexo
+  (:require
+   [clojure.string :as str]
+   [in.arthya.util.interface :as util]))
+
+(defn ->statement-map
+  "Convert a row (list) to map using statement header"
+  [row]
+  (zipmap ["Date & Time of Balance"
+           "Card type"
+           "Transaction ID"
+           "Transaction Details"
+           "Credit(in Rs.)"
+           "Debit(in Rs.)"
+           "Previous Balance(in Rs.)"
+           "New Balance(in Rs.)"]
+          row))
+
+(defn fix-date [s]
+  (-> s
+      (str/split #"\s")
+      first
+      util/fix-date))
+
+(defn ->posting
+  [row]
+  (let [id (get row "Transaction ID")
+        date (fix-date (get row "Date & Time of Balance"))
+        memo (get row "Transaction Details")
+        memo (str id "\n" memo)
+        credit (util/parse-currency-from-string (get row "Credit(in Rs.)"))
+        debit (util/parse-currency-from-string (get row "Debit(in Rs.)"))
+        amount (if (or (= 0 debit) (= 0.0 debit))
+                 credit
+                 (* -1 debit))]
+    {:date date
+     :memo memo
+     :amount amount
+     :account :sodexo}))
+
+(defn parse
+  [records]
+  (->> records
+       (map ->statement-map)
+       (map ->posting)))
