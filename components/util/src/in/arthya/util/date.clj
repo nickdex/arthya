98b35037e76e8ada5875e31e1f1a7c5f9142eeec
Nikhil Warke <nwarke@proton.me>
2024-02-16 06:07:56 +0530

    fix(util): Handle Sep edge case for date formatting
    t

--- components/util/src/in/arthya/util/date.clj
+++ components/util/src/in/arthya/util/date.clj
@@ -1,5 +1,7 @@
 (ns in.arthya.util.date
-  (:require [clj-time.format :as f]))
+  (:require
+   [clj-time.format :as f]
+   [clojure.string :as str]))
 
 (def output-format (f/formatter "yyyy/MM/dd"))
 
@@ -9,8 +11,13 @@
   (f/unparse output-format date))
 
 (defn fix-date
-  "Uses clj-time library formatter to convert string to LocalDate object, then unparse it to desired format"
+  "Uses clj-time library to convert string to LocalDate object, then change it to desired format.
+  Sanitizes the input in case source is Sending *Sep* with format *MMM*, the library expects *Sept*"
   [date-str {:keys [input]}]
-  (-> (f/formatter input)
-      (f/parse date-str)
-      format-date))
+  (let [sanitized-date-str (if (and (str/includes? input "MMM")
+                                (str/includes? date-str "Sep"))
+                         (str/replace date-str #"Sep" "Sept")
+                         date-str)]
+    (-> (f/formatter input)
+        (f/parse sanitized-date-str)
+        format-date)))
