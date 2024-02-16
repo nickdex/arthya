3d8d54fcd555700e9373e3a6030ed6b4b7fa0541
Nikhil Warke <nwarke@proton.me>
2024-02-16 06:53:45 +0530

    feat(parser): Add SBI Bank Parser

---
+++ components/parser/src/in/arthya/parser/sbi_bank.clj
@@ -0,0 +1,35 @@
+(ns in.arthya.parser.sbi-bank
+  (:require
+   [clojure.string :as str]
+   [in.arthya.util.interface :as util]))
+
+(defn ->statement-map
+  [row]
+  (zipmap ["Txn Date" "Value Date" "Description" "Ref No./Cheque No." "Debit" "Credit" "Balance"]
+          row))
+
+(defn ->posting
+  [row]
+  (let [date (util/fix-date (get row "Txn Date")
+                            {:input "d MMM yyyy"})
+        memo (str (get row "Description")
+                  "\n"
+                  (get row "Ref No./Cheque No."))
+        debit (util/parse-currency-from-string (get row "Debit"))
+        credit (util/parse-currency-from-string (get row "Credit"))
+        amount (if (or (= 0 debit) (= 0.0 debit))
+                 credit
+                 (* -1 debit))]
+    {:date date
+     :memo memo
+     :amount amount
+     :account :sbi-bank}))
+
+(defn parse
+  [records]
+  (->> records
+       (map #(str/split % #"\t"))
+       (map ->statement-map)
+       (map ->posting)))
+
+; LocalWords:  Txn Cheque MMM yyyy
