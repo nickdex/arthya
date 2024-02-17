55704e45e8cf61e79038007a31a78b6fe6833252
Nikhil Warke <nwarke@proton.me>
2024-02-17 22:49:29 +0530

    test(file-reader): Add test for reading text to equivalent clj map

--- components/file-reader/test/in/arthya/file_reader/ledger_test.clj
+++ components/file-reader/test/in/arthya/file_reader/ledger_test.clj
@@ -22,3 +22,15 @@
          (ledger/->posting
           ["Assets:Checking:Sodexo-6102  -239.0 INR"
            "; Test"]))))
+
+(deftest entry-test
+  (is (= {:comment ["446274839060" "SWIGGY"],
+          :date "2024/01/02",
+          :payee "Swiggy",
+          :postings [{:account "Assets:Checking:Sodexo-6102",
+                      :amount "-242.0",
+                      :currency "INR"}
+                     {:account "Expenses:Food"}],
+          :tags nil}
+         (ledger/->entry
+          ["2024/01/02 Swiggy" "; 446274839060" "; SWIGGY" "Assets:Checking:Sodexo-6102  -242.0 INR" "Expenses:Food"]))))
