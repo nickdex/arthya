8ffe09385fe111bb1cdda5c5bb387aa9d8d16ec2
Nikhil Warke <nwarke@proton.me>
2024-02-19 11:59:54 +0530

    feat(sms): Create SMS component

---
+++ components/sms/src/in/arthya/sms/interface.clj
@@ -0,0 +1,1 @@
+(ns in.arthya.sms.interface)
---
+++ components/sms/test/in/arthya/sms/interface_test.clj
@@ -0,0 +1,6 @@
+(ns in.arthya.sms.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [in.arthya.sms.interface :as sms]))
+
+(deftest dummy-test
+  (is (= 1 1)))
