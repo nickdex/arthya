d38b900890cf817657b3ae9d60ad7d0d5fc8ddf6
Nikhil Warke <nwarke@proton.me>
2024-02-21 09:20:29 +0530

    test(file-reader): Use vectors for sequence
    t,

--- components/file-reader/test/in/arthya/file_reader/core_test.clj
+++ components/file-reader/test/in/arthya/file_reader/core_test.clj
@@ -4,123 +4,123 @@
             [in.arthya.util.interface :as util]))
 
 (deftest trim-no-opts-test
-  (is (= '('(nil nil nil)
-            '("34"
+  (is (= [ [ nil nil nil ]
+            [ "34"
               "27/01/2024"
               "27/01/2024"
               "-"
               "UPI/439344021256/Deposit 1/9131161006@payt/State Bank Of I/ACD01HN4QG9TS2X69VSC"
               15000.0
               0.0
-              5705.94)
-            '("35"
+              5705.94 ]
+            [ "35"
               "27/01/2024"
               "27/01/2024"
               "-"
               "UPI/402784415982/bananas/q692910553@ybl/Yes Bank Ltd/ACD01HN53S95N29BXYZ70W0XRY"
               30.0
               0.0
-              5675.94)
-            '("Legends used in" "something")
-            '("Legends used in" "something"))
+              5675.94 ]
+            [ "Legends used in" "something" ]
+            [ "Legends used in" "something" ] ]
          (core/trim-rows
-           '('(nil nil nil)
-              '("34"
+           [ [ nil nil nil ]
+              [ "34"
                 "27/01/2024"
                 "27/01/2024"
                 "-"
                 "UPI/439344021256/Deposit 1/9131161006@payt/State Bank Of I/ACD01HN4QG9TS2X69VSC"
                 15000.0
                 0.0
-                5705.94)
-              '("35"
+                5705.94 ]
+              [ "35"
                 "27/01/2024"
                 "27/01/2024"
                 "-"
                 "UPI/402784415982/bananas/q692910553@ybl/Yes Bank Ltd/ACD01HN53S95N29BXYZ70W0XRY"
                 30.0
                 0.0
-                5675.94)
-              '("Legends used in" "something")
-              '("Legends used in" "something"))))))
+                5675.94 ]
+              [ "Legends used in" "something" ]
+              [ "Legends used in" "something" ] ]))))
 
 (deftest trim-terminate-pred-test
-  (is (= '('(nil nil nil)
-            '("34"
+  (is (= [ [ nil nil nil ]
+            [ "34"
               "27/01/2024"
               "27/01/2024"
               "-"
               "UPI/439344021256/Deposit 1/9131161006@payt/State Bank Of I/ACD01HN4QG9TS2X69VSC"
               15000.0
               0.0
-              5705.94)
-            '("35"
+              5705.94 ]
+            [ "35"
               "27/01/2024"
               "27/01/2024"
               "-"
               "UPI/402784415982/bananas/q692910553@ybl/Yes Bank Ltd/ACD01HN53S95N29BXYZ70W0XRY"
               30.0
               0.0
-              5675.94))
+              5675.94 ] ]
          (core/trim-rows
            {:terminate-pred #(not (util/contains-partial? % "legends"))}
-           '('(nil nil nil)
-              '("34"
+           [ [ nil nil nil ]
+              [ "34"
                 "27/01/2024"
                 "27/01/2024"
                 "-"
                 "UPI/439344021256/Deposit 1/9131161006@payt/State Bank Of I/ACD01HN4QG9TS2X69VSC"
                 15000.0
                 0.0
-                5705.94)
-              '("35"
+                5705.94 ]
+              [ "35"
                 "27/01/2024"
                 "27/01/2024"
                 "-"
                 "UPI/402784415982/bananas/q692910553@ybl/Yes Bank Ltd/ACD01HN53S95N29BXYZ70W0XRY"
                 30.0
                 0.0
-                5675.94)
-              '("Legends used in" "something")
-              '("Legends used in" "something"))))))
+                5675.94 ]
+              [ "Legends used in" "something" ]
+              [ "Legends used in" "something" ] ]))))
 
 (deftest trim-skip-start-test
-  (is (= '('("34"
+  (is (= [ [ "34"
              "27/01/2024"
              "27/01/2024"
              "-"
              "UPI/439344021256/Deposit 1/9131161006@payt/State Bank Of I/ACD01HN4QG9TS2X69VSC"
              15000.0
              0.0
-             5705.94)
-            '("35"
+             5705.94 ]
+            [ "35"
               "27/01/2024"
               "27/01/2024"
               "-"
               "UPI/402784415982/bananas/q692910553@ybl/Yes Bank Ltd/ACD01HN53S95N29BXYZ70W0XRY"
               30.0
               0.0
-              5675.94)
-            '("Legends used in" "something")
-            '("Legends used in" "something"))
+              5675.94 ]
+            [ "Legends used in" "something" ]
+            [ "Legends used in" "something" ] ]
          (core/trim-rows
            {:skip 1}
-           '('(nil nil nil)
-              '("34"
+           [ [ nil nil nil ]
+              [ "34"
                 "27/01/2024"
                 "27/01/2024"
                 "-"
                 "UPI/439344021256/Deposit 1/9131161006@payt/State Bank Of I/ACD01HN4QG9TS2X69VSC"
                 15000.0
                 0.0
-                5705.94)
-              '("35"
+                5705.94 ]
+              [ "35"
                 "27/01/2024"
                 "27/01/2024"
                 "-"
                 "UPI/402784415982/bananas/q692910553@ybl/Yes Bank Ltd/ACD01HN53S95N29BXYZ70W0XRY"
                 30.0
                 0.0
-                5675.94)
-              '("Legends used in" "something")
-              '("Legends used in" "something"))))))
+                5675.94 ]
+              [ "Legends used in" "something" ]
+              [ "Legends used in" "something" ] ]))))
