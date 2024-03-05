ce95b1ef8846db652bbee6a5cee8aeffe6c70dbb
Nikhil Warke <nwarke@proton.me>
2024-03-05 12:21:53 +0530

    refactor(payee): Move it to inference engine component

--- components/payee/src/in/arthya/payee/interface.clj
+++
@@ -1,7 +0,0 @@
-(ns in.arthya.payee.interface
- (:require [in.arthya.payee.core :as core]))
-
-(defn infer
-  "Find best possible match for payee provided by information from transaction. Return Unknown otherwise"
-  [transaction]
-  (core/infer transaction))
--- components/payee/test/in/arthya/payee/interface_test.clj
+++
@@ -1,7 +0,0 @@
-(ns in.arthya.payee.interface-test
-  (:require [clojure.test :as test :refer [deftest is]]
-            [in.arthya.payee.interface :as payee]))
-
-(deftest infer-test
-  (is (= "Cafe Vishala"
-         (payee/infer {:memo "8555202565\nCAFE VISHALA, BANGALORE, IN"}))))
