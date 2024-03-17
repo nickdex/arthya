5072032c1f85cc912dce3021b767fc4af5a8fdce
Nikhil Warke <nwarke@proton.me>
2024-03-17 14:17:47 +0530

    feat(util): Allow user to set output date format

--- components/util/src/in/arthya/util/date.clj
+++ components/util/src/in/arthya/util/date.clj
@@ -3,21 +3,21 @@
    [clj-time.format :as f]
    [clojure.string :as str]))
 
-(def output-format (f/formatter "yyyy/MM/dd"))
-
 (defn format-date
   "Converts the LocalDate object back into the desired output string format"
-  [date]
-  (f/unparse output-format date))
+  ([date output]
+   (f/unparse (f/formatter (if output
+                             output
+                             "yyyy/MM/dd")) date)))
 
 (defn fix-date
   "Uses clj-time library to convert string to LocalDate object, then change it to desired format.
   Sanitizes the input in case source is Sending *Sep* with format *MMM*, the library expects *Sept*"
-  [date-str {:keys [input]}]
+  [date-str {:keys [input output]}]
   (let [sanitized-date-str (if (and (str/includes? input "MMM")
-                                (str/includes? date-str "Sep"))
-                         (str/replace date-str #"Sep" "Sept")
-                         date-str)]
+                                    (str/includes? date-str "Sep"))
+                             (str/replace date-str #"Sep" "Sept")
+                             date-str)]
     (-> (f/formatter input)
         (f/parse sanitized-date-str)
-        format-date)))
+        (format-date output))))
