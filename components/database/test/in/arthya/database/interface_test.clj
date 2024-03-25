5d201fc1c8cef1fd1ec2e9fc5744de6ab9018089
Nikhil Warke <nwarke@proton.me>
2024-03-25 13:23:29 +0530

    feat(database): Add new database component
    Responsible for storing all data, including transactions.
    Also helps in querying the data
    
    database register

---
+++ components/database/test/in/arthya/database/interface_test.clj
@@ -0,0 +1,6 @@
+(ns in.arthya.database.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [in.arthya.database.interface :as database]))
+
+(deftest dummy-test
+  (is (= 1 1)))
