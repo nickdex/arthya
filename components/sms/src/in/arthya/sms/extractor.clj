b36d67adad3814d719b426a31572428adb478c8c
Nikhil Warke <nwarke@proton.me>
2024-03-25 15:41:37 +0530

    feat(sms): Ensure transaction doesn't contain nil values
    Add :source keyword for original message string

--- components/sms/src/in/arthya/sms/extractor.clj
+++ components/sms/src/in/arthya/sms/extractor.clj
@@ -2,18 +2,28 @@
   (:require
    [in.arthya.util.interface :as util]))
 
-(defn icici-card [msg]
-  (let [amount-spent (re-find #"(INR|USD)\s([\d,\.]+)" msg)
-        card-account (re-find #"ICICI Bank Card (\w+)" msg)
-        date-of-transaction (re-find #"\bon\s(\d{2}-\w{3}-\d{2})" msg)
-        payee (re-find #"\bat\s(.*?)\." msg)]
-    {:commodity (second amount-spent)
-     :quantity (* -1 (util/parse-currency (nth amount-spent 2)))
-     :account (str "Liabilities:CreditCard:" (subs (second card-account) 2))
-     :date (-> date-of-transaction
-               second
-               (util/fix-date {:input "dd-MMM-yy"}))
-     :payee (util/title-case (second payee))}))
+(defn create-txn [txn]
+  (into {} (filter (comp not nil? val) txn)))
+
+(defn icici-card [message]
+  (let [amount-spent (re-find #"(INR|USD)\s([\d,\.]+)" message)
+        card-account (re-find #"ICICI Bank Card (\w+)" message)
+        date-of-transaction (re-find #"\bon\s(\d{2}-\w{3}-\d{2})" message)
+        payee (re-find #"\bat\s(.*?)\." message)]
+    (create-txn {:account (str
+                           "Liabilities:CreditCard:"
+                           (subs (second card-account) 2)),
+                 :commodity (second amount-spent),
+                 :date (->
+                        date-of-transaction
+                        second
+                        (util/fix-date {:input "dd-MMM-yy"})),
+                 :payee (util/title-case (second payee)),
+                 :quantity (-> amount-spent
+                               (nth 2)
+                               util/parse-currency
+                               -)
+                 :source message})))
 
 (defn icici-bank
   [message]
@@ -22,69 +32,83 @@
         date-of-transaction (re-find #"on (\d{2}-\w{3}-\d{2})" message)
         credited-to (re-find #"; (.+) credited" message)
         upi-reference (re-find #"UPI:(\d+)" message)]
-    {:account (str "Assets:Checking:ICICI");;(second account-number)
-     :quantity (* -1 (util/parse-currency (second debited-amount)))
-     :date (-> date-of-transaction
-               second
-               (util/fix-date {:input "dd-MMM-yy"}))
-     :commodity "INR"
-     :payee (util/title-case (second credited-to))
-     :memo (str "UPI Ref: " (second upi-reference))}))
+    (create-txn {:account (str "Assets:Checking:ICICI"),
+                 :comment (str "UPI Ref - " (second upi-reference)),
+                 :commodity "INR",
+                 :date (-> date-of-transaction second (util/fix-date {:input "dd-MMM-yy"})),
+                 :payee (util/title-case (second credited-to)),
+                 :quantity (-> debited-amount
+                               second
+                               util/parse-currency
+                               -)
+                 :source message})))
 
-(defn pluxee-spent [s]
-  (let [amount-spent-re (re-find #"Rs\. (\d+\.\d+)" s)
-        card-re (re-find #", card no\.xx(\d+)," s)
-        datetime-re (re-find #", on (\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}) at " s)
-        payee-re (re-find #" at (.+)\. Avl bal" s)
-        available-balance-re (re-find #"Avl bal Rs\.(\d+\.\d+)\." s)]
-    {:quantity (* -1 (util/parse-currency (second amount-spent-re)))
-     :account (str "Assets:Checking:Sodexo:" (second card-re))
-     :commodity "INR"
-     :date (-> datetime-re
-               second
-               (util/fix-date {:input "dd-MM-yyyy HH:mm:ss"}))
-     :payee (second payee-re)
-     :available-balance (second available-balance-re)}))
+(defn pluxee-spent [message]
+  (let [amount-spent-re (re-find #"Rs\. (\d+\.\d+)" message)
+        card-re (re-find #", card no\.xx(\d+)," message)
+        datetime-re (re-find #", on (\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}) at " message)
+        payee-re (re-find #" at (.+)\. Avl bal" message)
+        available-balance-re (re-find #"Avl bal Rs\.(\d+\.\d+)\." message)]
+    (create-txn {:account (str "Assets:Checking:Sodexo:" (second card-re)),
+                 :comment (str "Available Balance - " (second available-balance-re))
+                 :commodity "INR",
+                 :date (->
+                        datetime-re
+                        second
+                        (util/fix-date {:input "dd-MM-yyyy HH:mm:ss"})),
+                 :payee (second payee-re),
+                 :quantity (-> amount-spent-re
+                               second
+                               util/parse-currency
+                               -)
+                 :source message})))
 
-(defn pluxee-spent-old [s]
-  (let [amount-spent-re (re-find #"Rs\. (\d+\.\d+)" s)
-        card-re (re-find #"Pluxee Card xx(\d+)" s)
-        datetime-re (re-find #"on (\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}) at " s)
-        payee-re (re-find #" at (.+?)\. Txn no\." s)
-        txn-no-re (re-find #"\. Txn no\. (\d+)\." s)
-        available-balance-re (re-find #"Avl bal is Rs\. (\d+\.\d+)\." s)]
-    {:quantity (second amount-spent-re)
-     :account (str "Assets:Checking:Sodexo:" (second card-re))
-     :commodity "INR"
-     :date (-> datetime-re
-               second
-               (util/fix-date {:input "MMM dd yyyy HH:mm:ss"}))
-     :payee (second payee-re)
-     :memo (str "Reference: " (second txn-no-re))
-     :available-balance (second available-balance-re)}))
+(defn pluxee-spent-old [message]
+  (let [amount-spent-re (re-find #"Rs\. (\d+\.\d+)" message)
+        card-re (re-find #"Pluxee Card xx(\d+)" message)
+        datetime-re (re-find #"on (\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}) at " message)
+        payee-re (re-find #" at (.+?)\. Txn no\." message)
+        txn-no-re (re-find #"\. Txn no\. (\d+)\." message)
+        available-balance-re (re-find #"Avl bal is Rs\. (\d+\.\d+)\." message)]
+    (create-txn {:account (str "Assets:Checking:Sodexo:" (second card-re)),
+                 :comment (str "Reference: " (second txn-no-re)
+                               "\n"
+                               "Available Balance - " (second available-balance-re)),
+                 :commodity "INR",
+                 :date (->
+                        datetime-re
+                        second
+                        (util/fix-date {:input "MMM dd yyyy HH:mm:ss"})),
+                 :payee (second payee-re),
+                 :quantity (-> amount-spent-re
+                               second
+                               util/parse-currency
+                               -)
+                 :source message})))
 
-(defn pluxee-refund [s]
-  (let [card-re (re-find #"Pluxee Card xx(\d+)" s)
-        credited-amount-re (re-find #"credited with INR (\d+\.\d+)" s)
-        credit-datetime-re (re-find #"on \w+ (\w+ \d+ \d+ \d+:\d+:\d+)" s)]
-    {:account (str "Assets:Checking:Sodexo:" (second card-re))
-     :commodity "INR"
-     :quantity (-> credited-amount-re
-                   second
-                   util/parse-currency
-                   (* -1))
-     :date (-> credit-datetime-re
-               second
-               (util/fix-date {:input  "MMM dd yyyy HH:mm:ss"}))}))
+(defn pluxee-refund [message]
+  (let [card-re (re-find #"Pluxee Card xx(\d+)" message)
+        credited-amount-re (re-find #"credited with INR (\d+\.\d+)" message)
+        credit-datetime-re (re-find #"on \w+ (\w+ \d+ \d+ \d+:\d+:\d+)" message)]
+    (create-txn {:account (str "Assets:Checking:Sodexo:" (second card-re)),
+                 :commodity "INR",
+                 :date (->
+                        credit-datetime-re
+                        second
+                        (util/fix-date {:input "MMM dd yyyy HH:mm:ss"})),
+                 :payee nil,
+                 :quantity (-> credited-amount-re second util/parse-currency)
+                 :source message})))
 
-(defn pluxee-topup [s]
-  {:account "Assets:Checking:Sodexo:6102"
-   :payee "Sumeru"
-   :commodity "INR"
-   :quantity 3500
-   :date (-> (re-find #"on \w+ (\w+ \d+ \d+ \d+:\d+:\d+)" s)
-             second
-             (util/fix-date {:input  "MMM dd yyyy HH:mm:ss"}))})
+(defn pluxee-topup [message]
+  (create-txn {:date (-> (re-find #"on \w+ (\w+ \d+ \d+ \d+:\d+:\d+)" message)
+                         second
+                         (util/fix-date {:input "MMM dd yyyy HH:mm:ss"})),
+               :payee "Sumeru",
+               :account "Assets:Checking:Sodexo:6102",
+               :quantity 3500.0,
+               :commodity "INR"
+               :source message}))
 
 (def extractor-map
   {"spent on ICICI Bank Card" icici-card
