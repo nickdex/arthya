d632867ee0c81af8026ee68c939196f320655159
Nikhil Warke <nwarke@proton.me>
2024-03-25 15:37:40 +0530

    feat(database): Add datascript layer
    datascript lets you model in a flexible way along with giving you powerful
    datalog query language

--- components/database/src/in/arthya/database/interface.clj
+++ components/database/src/in/arthya/database/interface.clj
@@ -1,1 +1,33 @@
-(ns in.arthya.database.interface)
+(ns in.arthya.database.interface
+  (:require
+   [clojure.edn :as edn]
+   [datascript.core :as d]))
+
+(def schema
+  {:postings {:db/cardinality​ :db.cardinality/many
+              :db/valueType :db.type/ref}})
+
+(defn persist!
+  "Write to disk"
+  ([database] (persist! "temp/arthya.edn" database))
+  ([file-path database]
+   (->> database
+        pr-str
+        (spit file-path))))
+
+(defn disk-db
+  "Reads from disk"
+  ([] (disk-db "temp/arthya.edn"))
+  ([file-path]
+   (->> file-path
+        slurp
+        (edn/read-string
+         {:readers d/data-readers}))))
+
+(def transact! d/transact!)
+
+(defn q
+  [query & inputs]
+  (apply d/q query inputs))
+
+(def create-conn d/create-conn)
