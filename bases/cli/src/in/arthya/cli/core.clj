bd5ff3496708235c24e9ffd4d3d43ec5b83ed3a4
Nikhil Warke <nwarke@proton.me>
2024-05-09 10:05:50 +0530

    feat(cli): Add cli base to create cli tools

---
+++ bases/cli/src/in/arthya/cli/core.clj
@@ -0,0 +1,1 @@
+(ns in.arthya.cli.core)
---
+++ bases/cli/test/in/arthya/cli/core_test.clj
@@ -0,0 +1,6 @@
+(ns in.arthya.cli.core-test
+  (:require [clojure.test :as test :refer :all]
+            [in.arthya.cli.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
