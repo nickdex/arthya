3381601f1d58641389005cbe6fda34620bd16826
Nikhil Warke <nwarke@proton.me>
2024-05-09 17:51:28 +0530

    feat(cli): Add components dependencies
    So that export sms can work

--- bases/cli/src/in/arthya/cli/core.clj
+++ bases/cli/src/in/arthya/cli/core.clj
@@ -1,6 +1,28 @@
 (ns in.arthya.cli.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [in.arthya.hledger.interface :as hl]
+   [in.arthya.inference-engine.interface :as ie]
+   [in.arthya.sms.interface :as sms]))
+
+(defn export-sms [{:keys [file-path]
+                   :or {file-path  "/Users/nik/Downloads/sms.edn"}
+                   :as opts}]
+  (let [records (->>
+                 (sms/read-sms opts)
+                 (map #(if-let [payee (ie/infer-payee %)]
+                         (assoc % :payee payee)
+                         %))
+                 (map #(if-let [account (ie/infer-account %)]
+                         (assoc % :xaccount account)
+                         %))
+                 (map hl/->hledger-transaction))]
+    (println (count records) " exported to " file-path)
+    (->> records
+         pr-str
+         (spit file-path))))
 
 (defn -main [& args]
-  (println "Hello")
+  (println "Starting export of sms")
+  (export-sms {:limit 500})
   (System/exit 0))
